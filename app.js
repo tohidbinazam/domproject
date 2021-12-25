5a754f217f0368133c0c4aba7a2e493d8c3114e4
tohidbinazam <tohidbinazamsunny1@gmail.com>
2021-12-26 04:56:27 +0600

    devupdate

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const hello = document.querySelector('html');
-console.log(hello.innerHTML);
-
-// console.log(document.domain);
+const row = document.querySelector('.row');
 
 
-
+devs_data.map((data) => {
+    row.innerHTML += `
+     <div class="col-xl-4">
+        <div class="dev_team">
+            <img src="${data.img}" alt="">
+            <h2 class="name">${data.name}</h2>
+            <h3 class="skill">${data.Skill}</h3>
+            <h3 class="address">${data.address}</h3>
+            <p>Hello, consectetur adipisicing elit. Sapiente quaerat architecto incidunt perspiciatis odio molestiae repudiandae optio laborum officia necessitatibus?</p>
+        </div>
+     </div>
+    `
+});
 
 
 
---
+++ data.js
@@ -0,0 +1,38 @@
+const devs_data = [
+    {
+        name : 'Tohid Bin Azam',
+        Skill : 'PHP',
+        address: 'Khulna',
+        img: 'https://www.jeancoutu.com/globalassets/revamp/photo/conseils-photo/20160302-01-reseaux-sociaux-profil/photo-profil_301783868.jpg'
+    },
+    {
+        name : 'Khalid Farhan',
+        Skill : 'JAVA',
+        address: 'Barisal',
+        img: 'https://image.shutterstock.com/image-photo/close-photo-amazing-attractive-he-260nw-1343304206.jpg'
+    },
+    {
+        name : 'Freelancer Nasim',
+        Skill : 'LARAVEL',
+        address: 'Dhaka',
+        img: 'https://shotkit.com/wp-content/uploads/2021/06/cool-profile-pic-matheus-ferrero.jpeg'
+    },
+    {
+        name : 'Ashraful Haq',
+        Skill : 'Ruby on Rails',
+        address: 'Gopalgong',
+        img: 'https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg'
+    },
+    {
+        name : 'Shajid Bhuiyan',
+        Skill : 'MERN',
+        address: 'Magura',
+        img: 'https://image.shutterstock.com/image-photo/profile-picture-young-handsome-man-260nw-1795243888.jpg'
+    },
+    {
+        name : 'Solaiman Shukhon',
+        Skill : 'GO',
+        address: 'Sylhet',
+        img: 'https://image.shutterstock.com/image-photo/close-head-shot-confident-serious-260nw-1481322794.jpg'
+    },
+];
