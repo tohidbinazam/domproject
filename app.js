3ec114a4992d42ebcb0cb59df9d069a641a5039c
tohidbinazam <tohidbinazamsunny1@gmail.com>
2021-12-28 19:35:02 +0600

    Massage done

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
-const row = document.querySelector('.row');
+const js_dvs = document.querySelector('.js_dvs');
+const js_student = document.querySelector('.js_student');
+const js_number = document.querySelector('table tbody');
 
 
-devs_data.map((data) => {
-    row.innerHTML += `
-     <div class="col-xl-4">
+for( let i = 0;  i < 6 ; i++) {
+    js_dvs.innerHTML += `
+    <div class="col-xl-4">
         <div class="dev_team">
-            <img src="${data.img}" alt="">
-            <h2 class="name">${data.name}</h2>
-            <h3 class="skill">${data.Skill}</h3>
-            <h3 class="address">${data.address}</h3>
-            <p>Hello, consectetur adipisicing elit. Sapiente quaerat architecto incidunt perspiciatis odio molestiae repudiandae optio laborum officia necessitatibus?</p>
+            <img src="${devs_stu_phone_data[i].img}" alt="">
+            <h2 class="name">${devs_stu_phone_data[i].name}</h2>
+            <h3 class="skill">${devs_stu_phone_data[i].Skill}</h3>
+            <h3 class="address">${devs_stu_phone_data[i].address}</h3>
+            <p>Hello there, My name is ${devs_stu_phone_data[i].name}. I am a ${devs_stu_phone_data[i].Skill} developer form ${devs_stu_phone_data[i].address}. Now i read in class ${devs_stu_phone_data[i].class} and i am ${devs_stu_phone_data[i].age} years old. My last exam result is CGPA ${devs_stu_phone_data[i].cgpa}. At last my contact number is +880${devs_stu_phone_data[i].number}</p>
         </div>
-     </div>
+    </div>
     `
-});
+};
+
+
+devs_stu_phone_data.map((data) => {
+    js_student.innerHTML += `
+    <div class="col-xl-3">
+        <div class="student">
+            <img src="${data.img}" alt="">
+            <h3 class="name">Name: ${data.name}</h3>
+            <h3 class="age">Age: ${data.age}</h3>
+            <h3 class="class">Class: ${data.class}</h3>
+            <h3 class="result">CGPA : ${data.cgpa}</h3>
+        </div>
+    </div>
+    `
+})
+
+devs_stu_phone_data.map((data, index) => {
+    js_number.innerHTML += `
+    <tr>
+        <th>${index + 1}</th>
+        <td>${data.name}</td>
+        <td>+880${data.number}</td>
+        <td><img class="table_img" src="${data.img}" alt=""></td>
+    </tr>
+    `
+})
 
 
 
--- data.js
+++ data.js
@@ -1,38 +1,83 @@
-const devs_data = [
+const devs_stu_phone_data = [
     {
         name : 'Tohid Bin Azam',
         Skill : 'PHP',
         address: 'Khulna',
-        img: 'https://www.jeancoutu.com/globalassets/revamp/photo/conseils-photo/20160302-01-reseaux-sociaux-profil/photo-profil_301783868.jpg'
+        age: 23,
+        class: 12,
+        cgpa: 4.96,
+        img: 'https://www.jeancoutu.com/globalassets/revamp/photo/conseils-photo/20160302-01-reseaux-sociaux-profil/photo-profil_301783868.jpg',
+        number: 1986957528
+        
     },
     {
         name : 'Khalid Farhan',
         Skill : 'JAVA',
         address: 'Barisal',
-        img: 'https://image.shutterstock.com/image-photo/close-photo-amazing-attractive-he-260nw-1343304206.jpg'
+        age: 18,
+        class: 10,
+        cgpa: 2.86,
+        img: 'https://image.shutterstock.com/image-photo/close-photo-amazing-attractive-he-260nw-1343304206.jpg',
+        number: 1938409463
     },
     {
-        name : 'Freelancer Nasim',
+        name : 'Sanjida Islam',
         Skill : 'LARAVEL',
         address: 'Dhaka',
-        img: 'https://shotkit.com/wp-content/uploads/2021/06/cool-profile-pic-matheus-ferrero.jpeg'
+        age: 13,
+        class: 3,
+        cgpa: 4.96,
+        img: 'https://shotkit.com/wp-content/uploads/2021/06/cool-profile-pic-matheus-ferrero.jpeg',
+        number: 1859615720
     },
     {
         name : 'Ashraful Haq',
         Skill : 'Ruby on Rails',
         address: 'Gopalgong',
-        img: 'https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg'
+        age: 15,
+        class: 9,
+        cgpa: 3.58,
+        img: 'https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg',
+        number: 1350489010
     },
     {
         name : 'Shajid Bhuiyan',
         Skill : 'MERN',
         address: 'Magura',
-        img: 'https://image.shutterstock.com/image-photo/profile-picture-young-handsome-man-260nw-1795243888.jpg'
+        age: 18,
+        class: 8,
+        cgpa: 3.99,
+        img: 'https://image.shutterstock.com/image-photo/profile-picture-young-handsome-man-260nw-1795243888.jpg',
+        number: 1452967242
+    },
+    {
+        name : 'Mahayjabin Khan',
+        Skill : 'python',
+        address: 'Sylhet',
+        age: 11,
+        class: 5,
+        cgpa: 4.88,
+        img: 'https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg',
+        number: 1635814208
     },
     {
         name : 'Solaiman Shukhon',
+        Skill : 'LARAVEL',
+        address: 'Khulna',
+        age: 8,
+        class: 3,
+        cgpa: 2.68,
+        img: 'https://image.shutterstock.com/image-photo/close-head-shot-confident-serious-260nw-1481322794.jpg',
+        number: 1935715300
+    },
+    {
+        name : 'Anamika khatun',
         Skill : 'GO',
-        address: 'Sylhet',
-        img: 'https://image.shutterstock.com/image-photo/close-head-shot-confident-serious-260nw-1481322794.jpg'
+        address: 'barisal',
+        age: 12,
+        class: 4,
+        cgpa: 4.68,
+        img: 'https://img.freepik.com/free-photo/teenager-student-girl-yellow-pointing-finger-side_1368-40175.jpg?size=626&ext=jpg',
+        number: 1720000482
     },
 ];
